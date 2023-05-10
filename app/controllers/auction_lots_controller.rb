358d9100764e86b3319a9b169e3bc47fdec2ffdc
sorwalisson <sorwalisson@gmail.com>
2023-05-09 21:18:14 -0300

    different admins confirmations for auction_lot

--- app/controllers/auction_lots_controller.rb
+++ app/controllers/auction_lots_controller.rb
@@ -1,6 +1,6 @@
 class AuctionLotsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :awaiting_confirmation]
-  before_action :authenticate_user_admin, only: [:new, :create, :edit, :update, :awaiting_confirmation]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :awaiting_confirmation, :confirmed]
+  before_action :authenticate_user_admin, only: [:new, :create, :edit, :update, :awaiting_confirmation, :confirmed]
   def show
     set_lot_and_verify
   end
@@ -42,6 +42,16 @@ class AuctionLotsController < ApplicationController
     redirect_to @auction_lot, notice: t('status_msg.auction.set_awaiting')
   end
 
+  def confirmed
+    set_for_confirmed
+    if current_user.id == @auction_lot.user_id
+      redirect_to @auction_lot, notice: t('status_msg.auction.not_set_confirmed')
+    else 
+      @auction_lot.update(status: :confirmed)
+      redirect_to @auction_lot, notice: t('status_msg.auction.set_confirmed')
+    end
+  end
+
   private
   def auction_lot_params
     params.require(:auction_lot).permit(:auction_code, :starting_time, :ending_time, :starting_bid, :bid_difference)
@@ -56,6 +66,12 @@ class AuctionLotsController < ApplicationController
   def set_lot_for_update
     @auction_lot = AuctionLot.find_by(id: params[:id])
     if @auction_lot.status != "draft" then redirect_to root_path, notice: t('status_msg.auction.acess_denied') end
+    @auction_lot
+  end
+
+  def set_for_confirmed
+    @auction_lot = AuctionLot.find_by(id: params[:id])
+    if @auction_lot.status != "awaiting_confirmation" then redirect_to root_path, notice: t('status_msg.auction.acess_denied') end
     @auction_lot
   end
 end
--- spec/system/auction_lot/admin_changes_auction_status_spec.rb
+++ spec/system/auction_lot/admin_changes_auction_status_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
+
 describe 'admin changes status of a auction' do
   it 'admin changes status from draft to awaiting confirmation' do
     new_admin = User.create!(name: "Walisson", email: "sorwalisson@leilaodogalpao.com.br", password: "password", address: "Avenida Fernandes Lima, 35",
@@ -14,7 +15,7 @@ describe 'admin changes status of a auction' do
     visit root_path
     click_on 'Admin Menu'
     within('div#draft') do
-    click_on 'bbc693221'
+      click_on 'bbc693221'
     end
     click_on "Change status to awaiting confirmation"
 
@@ -22,6 +23,56 @@ describe 'admin changes status of a auction' do
     expect(page).to have_content "The auction is now set as awaiting confirmation"
     expect(page).to have_content "Status: awaiting_confirmation"
     expect(page).to_not have_button "Change status to awaiting confirmation"
+    expect(page).to have_button "Change status to confirmed"
+  end
+
+  it 'admin changes status to confirmed, a admin that is different from the one who created the auction' do
+    new_admin = User.create!(name: "Walisson", email: "sorwalisson@leilaodogalpao.com.br", password: "password", address: "Avenida Fernandes Lima, 35",
+                            zip: "57000-100", cpf: "09814576492")
+    second_admin  = User.create!(name: "stuart", email: "stuart@leilaodogalpao.com.br", password: "password", address: "Avenida Getulio Vargas, 50", 
+                                zip: "50000-500", cpf: "22542452016")
+    new_auction = AuctionLot.create!(starting_time: 5.hours.from_now, ending_time: 3.days.from_now, auction_code: "bbc693221", starting_bid: 5000, bid_difference: 5, user_id: new_admin.id)
+    new_product = Item.create!(name: "Ryzen 7 5800x", description: "Ryzen 7 5800x, A 3.8GHz processor with 8 cores with the new AMD technology AMD 3D V-Cache and Zen 3 architecture.",
+                              weight: 247, height: 3, width: 8, depth: 3,
+                              img_url: "https://images.kabum.com.br/produtos/fotos/129459/processador-amd-ryzen-9-5900x-cache-70mb-3-7ghz-4-8ghz-max-turbo-am4-100-100000063wof_1602600708_gg.jpg",
+                              category: 0, auction_lot_id: new_auction.id)
+    
+    new_auction.update(status: :awaiting_confirmation)
+    login_as(second_admin)
+    visit root_path
+    click_on 'Admin Menu'
+    within('div#awaiting_confirmation') do
+      click_on 'bbc693221'
+    end
+    click_on "Change status to confirmed"
+
+    expect(current_path).to eq auction_lot_path(id: new_auction.id)
+    expect(page).to have_content "The auction was set as confirmed"
+    expect(page).to have_content "Status: confirmed"
+    expect(page).to_not have_button "Change status to confirmed"
+  end
+
+  it 'admin tries to change status to confirmed, but it fails because its the same admin who created the auction' do
+    new_admin = User.create!(name: "Walisson", email: "sorwalisson@leilaodogalpao.com.br", password: "password", address: "Avenida Fernandes Lima, 35",
+                             zip: "57000-100", cpf: "09814576492")
+    new_auction = AuctionLot.create!(starting_time: 5.hours.from_now, ending_time: 3.days.from_now, auction_code: "bbc693221", starting_bid: 5000, bid_difference: 5, user_id: new_admin.id)
+    new_product = Item.create!(name: "Ryzen 7 5800x", description: "Ryzen 7 5800x, A 3.8GHz processor with 8 cores with the new AMD technology AMD 3D V-Cache and Zen 3 architecture.",
+                               weight: 247, height: 3, width: 8, depth: 3,
+                               img_url: "https://images.kabum.com.br/produtos/fotos/129459/processador-amd-ryzen-9-5900x-cache-70mb-3-7ghz-4-8ghz-max-turbo-am4-100-100000063wof_1602600708_gg.jpg",
+                               category: 0, auction_lot_id: new_auction.id)
+                             
+    new_auction.update(status: :awaiting_confirmation)
+    login_as(new_admin)
+    visit root_path
+    click_on 'Admin Menu'
+    within('div#awaiting_confirmation') do
+      click_on 'bbc693221'
+    end
+    click_on "Change status to confirmed"
+
+    expect(current_path).to eq auction_lot_path(id: new_auction.id)
+    expect(page).to have_content "Could not change status to confirmed, because the admin is the same as the creater of the auction."
+    expect(page).to have_content "Status: awaiting_confirmation"
     expect(page).to have_button "Change status to confirmed"
   end
 end
